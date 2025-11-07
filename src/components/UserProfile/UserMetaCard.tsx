e2157b79ea4cb1d7c48c0eac0760b082ae94512d
eyobed101 <eyocjjazz101@gmail.com>
2025-11-08 00:28:46 +0300

    Fix Button import path case sensitivity

--- src/components/UserProfile/UserMetaCard.tsx
+++ src/components/UserProfile/UserMetaCard.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 import { useModal } from "../../hooks/useModal";
 import { Modal } from "../ui/modal";
-import Button from "../ui/Button/Button"; // Fixed: Button directory is capitalized
+import Button from "../ui/Button/Button";
 import Input from "../form/input/InputField";
 import Label from "../form/Label";
 import axiosInstance from "../../utils/axiosInstance";
